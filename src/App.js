20e657aec30943028ee9a5cc704eb9ad931d9702
Alvaro Condori <condorialvaro95@gmail.com>
2025-02-08 22:46:51 -0300

    que necesitas para aprender react.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './platzi.webp';
-import './App.css';
+import logo from "./platzi.webp";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edita el archivo <code>src/App.js</code> y guarda para recargar.
         </p>
         <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          className='App-link'
+          href='https://platzi.com/reactjs'
+          target='_blank'
+          rel='noopener noreferrer'>
+          Aprendemos React
         </a>
       </header>
     </div>
+  );
+}
+
+function TodoItem() {
+  return (
+    <li>
+      <span>V</span>
+      <p>Llorar con la Llorona</p>
+      <span>X</span>
+    </li>
   );
 }
 
