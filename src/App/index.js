b88be00f98ab4e29efad7655d9d79e8a91c6f819
Alvaro Condori <condorialvaro95@gmail.com>
2025-02-18 23:39:28 -0300

    tips de naming y abstraccion

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,6 @@
 import React from "react";
-import {TodoCounter} from "../TodoCounter";
-import {TodoSearch} from "../TodoSearch";
-import {TodoList} from "../TodoList";
-import {TodoItem} from "../TodoItem";
-import {CreateTodoButton} from "../CreateTodoButton";
 import {useLocalStorage} from "./useLocalStorage";
+import {AppUI} from "./AppUI";
 
 // const defaultTodos = [
 //   {text: "Cortar cebolla", completed: true},
@@ -45,24 +41,15 @@ function App() {
   };
 
   return (
-    <>
-      <TodoCounter completed={completedTodos} total={totalTodos} />
-      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
-
-      <TodoList>
-        {searchedTodos.map((todo) => (
-          <TodoItem
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={() => deleteTodo(todo.text)}
-          />
-        ))}
-      </TodoList>
-
-      <CreateTodoButton />
-    </>
+    <AppUI
+      completedTodos={completedTodos}
+      totalTodos={totalTodos}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+    />
   );
 }
 
